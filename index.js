9bfefc5dcc9084e353723ffeb7326acf875f9724
ParkMD7 <parkermdinsmore@gmail.com>
2019-02-13 13:47:49 -0500

    added CORS configuration to backend

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const http = require('http');
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 
 
@@ -15,12 +16,13 @@ mongoose.connect('mongodb://localhost:auth/authentication', {useNewUrlParser: tr
 // App Setup NOTE: morgan and bodyParser are middlewares in express -> any incoming request into our server will be passed into both of these by default via app.use
 const app = express(); // create instance of our app
 app.use(morgan('combined')); // this is a logging framework that logs incoming requests and is used mostly for debugging
+app.use(cors()); // default use of CORS module -> enables our express application to accept any requets at the moment 
 app.use(bodyParser.json({ type: '*/*' })); // this is used to parse incoming requests into JSON no matter what the request type is
 router(app);
 
 
 // Server Setup NOTE: nodemon will auto restart the server if we make any changes to the code
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const server = http.createServer(app)
 server.listen(port);
 console.log('Server listening on:', port)
